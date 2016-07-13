15125d26251a279e4e27dd265f721613c33eceeb
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-13 20:16:42 +0300

    Implemented simple_sum. Made corresponding tests pass.

--- Module_1-05_calc/src/source.c
+++ Module_1-05_calc/src/source.c
@@ -2,13 +2,25 @@
 #include <math.h>
 #include "source.h"
 
-
-
-void simple_sum(void)
-{
+void simple_sum(void) {
+   int a, b;
+   
+   scanf("%d %d", &a, &b);
+   
+   print_sum(a, b);
 }
 
+/**
+ * Sums up the two given integers and prints them to the console in the defined format. 
+ * 
+ * @param a
+ * @param b
+ */
+void print_sum(int a, int b) {
+    int result = a + b;
+    printf("%d + %d = %d\n", a, b, result);
+}
 
-void simple_math(void)
-{
+void simple_math(void) {
+    
 }
