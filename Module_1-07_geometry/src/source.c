c19db4912dd6140bc03337ef2e8ebf58076cfc3e
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-13 21:18:40 +0300

    Implemented multi_table function.

--- Module_1-07_geometry/src/source.c
+++ Module_1-07_geometry/src/source.c
@@ -2,9 +2,14 @@
 #include <math.h>
 #include "source.h"
 
-
-void multi_table(unsigned int xsize, unsigned int ysize)
-{
+void multi_table(unsigned int xsize, unsigned int ysize) {
+    for(int i = 1; i <= ysize; i++) {
+        for(int j = 1; j <= xsize; j++) {
+            printf("%4d", j * i);
+        }
+        
+        printf("\n");
+    }
 }
 
 
