6db9528d279990da2af73d3ce6455a5b1109f1d5
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-13 19:41:14 +0300

    Implemented and documented vectorlength function. Made all tests pass.

--- Module_1-04_func/src/source.c
+++ Module_1-04_func/src/source.c
@@ -1,2 +1,15 @@
 #include <math.h>
 
+/**
+ * Calculates the length of 3 dimensional Euclidean vector based on the three given parameters.
+ * 
+ * @param a
+ * @param b
+ * @param c
+ * @return The lenght of the vector as a double value. 
+ */
+double vectorlength(double a, double b, double c) {
+    double result = sqrt(a * a + b * b + c * c);
+    
+    return result; 
+}
