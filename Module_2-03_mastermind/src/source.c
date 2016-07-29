d43e29a92e23d95d288a9050bb13c2fa995397c8
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-29 23:18:16 +0300

    Implemented mastermind()

--- Module_2-03_mastermind/src/source.c
+++ Module_2-03_mastermind/src/source.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 #include "source.h"
-
+#include <stdbool.h>
 
 #ifndef MAWKKE
-// longer solution by Pasi
 
 /* 03-mastermind
  */
-void mastermind(const int *solution, const int *guess, char *result, unsigned int len)
-{
-    (void) solution;
-    (void) guess;
-    (void) result;
-    (void) len;
+bool contains(int *array, int number, unsigned int length) {
+    for (int i = 0; i < length; i++) {
+        if (*(array + i) == number) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void mastermind(const int *solution, const int *guess, char *result, unsigned int len) {
+    int next_guess;
+    for (int i = 0; i < len; i++) {
+        next_guess = *(guess + i);
+        
+        if (*(solution + i) == next_guess)
+            *(result + i) = '+';
+        else if (contains(solution, next_guess, len))
+            *(result + i) = '*';
+        else
+            *(result + i) = '-';
+    }
 }
 #endif
