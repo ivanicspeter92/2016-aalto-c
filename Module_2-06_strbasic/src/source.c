b121b6e55a7781056444476f4cf4d6739fea79bb
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-30 10:11:49 +0300

    Implemented count_alpha()

--- Module_2-06_strbasic/src/source.c
+++ Module_2-06_strbasic/src/source.c
@@ -1,14 +1,22 @@
-#include "source.h"
+#include <string.h>
 
+#include "source.h"
+#include "ctype.h"
 
 /* Count Alpha
  * Count number of alphabetic characters in the given string <str>,
  * and return the count
  */
-int count_alpha(const char *str)
-{
-    (void) str;
-    return 0;  // replace this
+int count_alpha(const char *str) {
+    int count = 0;
+    
+    for (int i = 0; i < strlen(str); i++) {
+        if (isalpha(*(str + i)) != 0) {
+            count++;
+        }
+    }
+    
+    return count;
 }
 
 
