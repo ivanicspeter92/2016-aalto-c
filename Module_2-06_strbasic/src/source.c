1cf7af55c0c5ff4debf1cf52c260d89ddb1513c2
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-30 10:48:46 +0300

    Implemented count_substr()

--- Module_2-06_strbasic/src/source.c
+++ Module_2-06_strbasic/src/source.c
@@ -24,11 +24,22 @@ int count_alpha(const char *str) {
  * Count number of occurances of substring <sub> in string <str>,
  * and return the count.
  */
-int count_substr(const char *str, const char *sub)
-{
-    (void) str;
-    (void) sub;
-    return 0;  // replace this
+int count_substr(const char *str, const char *sub) {
+    int count = 0;
+    int substring_pointer = 0;
+    char next_in_str, next_in_sub;
+    for (int i = 0; i < strlen(str); i++) {
+        next_in_str = *(str + i);
+        next_in_sub = *(sub + substring_pointer);
+        if (next_in_str == next_in_sub) {
+            substring_pointer++;
+            if (substring_pointer == strlen(sub)) {
+                count++;
+                substring_pointer = 0;
+            }  
+        } else 
+            substring_pointer = 0;
+    }
+    
+    return count;
 }
-
-
