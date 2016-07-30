de1c9ad5849bddfe3bdddfc25c163c5790461c02
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-30 19:39:47 +0300

    Implemented str_to_strarray()

--- Module_2-09_stringarray/src/source.c
+++ Module_2-09_stringarray/src/source.c
@@ -1,5 +1,6 @@
 #include "source.h"
 #include <stdlib.h>
+#include <string.h>
 
 /* Print string array, last element is NULL */
 /* Parameters:
@@ -18,7 +19,21 @@ void print_strarray(char *array[]) {
  * string: string to be cut into parts and placed into the array, 
    remember to add ending zeros '\0' for strings and NULL to the end of the whole array!
  * arr: ready-made array that the strings will be put into */
-void str_to_strarray(char* string, char** arr)
-{
-    (void) string; (void) arr;
+void str_to_strarray(char* string, char** arr) {
+    char delimiter = ' ';
+    int i;
+    int count = 0; 
+    int j = 0;
+    for(i = 0; i < strlen(string); i++) {
+        if (*(string + i) == delimiter) {
+            arr[count][j] = '\0';
+            count++;
+            j = 0;
+        } else {
+            arr[count][j] = *(string + i);
+            j++;
+        }
+    }
+    arr[count][j] = '\0';
+    arr[count + 1] = NULL;
 }
