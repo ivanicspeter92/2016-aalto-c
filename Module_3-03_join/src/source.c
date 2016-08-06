772e216c4f1c58a47fdef4b3b3d93532773901b9
Peter Ivanics <peter.ivanics@reslink.fi>
2016-08-06 16:20:42 +0300

    Added includes

--- Module_3-03_join/src/source.c
+++ Module_3-03_join/src/source.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include "source.h"
+
 int* join_arrays(unsigned int numbers1, int* pointer1, unsigned int numbers2, int* pointer2, unsigned int numbers3, int* pointer3) {
-    return -1;
+    
 }
