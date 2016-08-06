35e5cf4382fdcb4fb79698f4ac02b37235c91368
Peter Ivanics <peter.ivanics@reslink.fi>
2016-08-06 16:49:42 +0300

    Refactoring.

--- Module_3-03_join/src/source.c
+++ Module_3-03_join/src/source.c
@@ -1,24 +1,33 @@
 #include <stdlib.h>
 #include "source.h"
 
+void join_array(unsigned int number_of_elements, int* pointer_to_array, int* pointer_to_result) {
+    for (int i = 0; i < number_of_elements; i++) {
+        pointer_to_result[i] = pointer_to_array[i];
+    }
+}
+
 int* join_arrays(unsigned int numbers1, int* pointer1, unsigned int numbers2, int* pointer2, unsigned int numbers3, int* pointer3) {
     int totalelements = numbers1 + numbers2 + numbers3;
-    int* result = malloc(totalelements * sizeof(int));
+    int* result = (int*)malloc(totalelements * sizeof(int));
     if (result != NULL) {
-        int count = 0;
+        join_array(numbers1, pointer1, result + 0);
+        join_array(numbers2, pointer2, result + numbers1);
+        join_array(numbers3, pointer3, result + numbers1 + numbers2);
         
-        for (int i = 0; i < numbers1; i++) {
-            result[count] = pointer1[i];
-            count++;
-        }
-        for (int i = 0; i < numbers2; i++) {
-            result[count] = pointer2[i];
-            count++;
-        }
-        for (int i = 0; i < numbers3; i++) {
-            result[count] = pointer3[i];
-            count++;
-        }
+//        int count = 0;
+//        for (int i = 0; i < numbers1; i++) {
+//            result[count] = pointer1[i];
+//            count++;
+//        }
+//        for (int i = 0; i < numbers2; i++) {
+//            result[count] = pointer2[i];
+//            count++;
+//        }
+//        for (int i = 0; i < numbers3; i++) {
+//            result[count] = pointer3[i];
+//            count++;
+//        }
         
         return result;
     }
