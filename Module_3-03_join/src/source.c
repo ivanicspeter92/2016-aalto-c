dd0135b53aaee9342d358c93228d8f20324bec49
Peter Ivanics <peter.ivanics@reslink.fi>
2016-08-06 16:18:22 +0300

    Added int* join_arrays(unsigned int numbers1, int* pointer1, unsigned int numbers2, int* pointer2, unsigned int numbers3, int* pointer3) definition

---
+++ Module_3-03_join/src/source.c
@@ -0,0 +1,3 @@
+int* join_arrays(unsigned int numbers1, int* pointer1, unsigned int numbers2, int* pointer2, unsigned int numbers3, int* pointer3) {
+    return -1;
+}
---
+++ Module_3-03_join/src/source.h
@@ -0,0 +1,1 @@
+int* join_arrays(unsigned int numbers1, int* pointer1, unsigned int numbers2, int* pointer2, unsigned int numbers3, int* pointer3);
