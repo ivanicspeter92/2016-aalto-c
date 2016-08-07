f2823d1c4172c9f0e5f51b16ce31245c23a8ddd8
Peter Ivanics <peter.ivanics@reslink.fi>
2016-08-07 09:02:35 +0300

    Implemented create_vessel()

--- Module_3-04_vessel/src/source.c
+++ Module_3-04_vessel/src/source.c
@@ -1,13 +1,23 @@
+#include <stdlib.h>
+
 #include "source.h"
 
-
 struct vessel create_vessel(const char *p_name,
-			    double p_length,
-			    double p_depth,
-			    struct cargo p_crg)
-{
+        double p_length,
+        double p_depth,
+        struct cargo p_crg) {
+    struct vessel result;
+    int name_length = 30;
+
+    strcpy(result.name, p_name);
+    result.name[name_length] = '\0';
+
+    result.length = p_length;
+    result.depth = p_depth;
+    result.crg = p_crg;
+
+    return result;
 }
 
-void print_vessel(const struct vessel *ship)
-{
+void print_vessel(const struct vessel *ship) {
 }
