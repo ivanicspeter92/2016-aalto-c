0a858d9d55c42bb465a6f25fac9a9bc71d5058b9
Peter Ivanics <peter.ivanics@reslink.fi>
2016-08-13 23:17:42 +0300

    Implemented line_count()

--- Module_5-02_stats/src/filestats.c
+++ Module_5-02_stats/src/filestats.c
@@ -1,5 +1,7 @@
 #include "filestats.h"
-
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 /* Returns the line count in given file
  * 
@@ -8,10 +10,35 @@
  * 
  * Returns:
  * The number of lines in file. */
-int line_count(const char *filename)
-{
-    (void) filename;
-    return 0;
+int line_count(const char *filename) {
+    FILE* filestream = fopen(filename, "r");
+    
+    if (filestream != NULL) {
+        char next_character_in_file;
+        int line_count = 1, character_count = 0;
+        bool is_last_line_empty = true;
+        
+        while((next_character_in_file = fgetc(filestream)) != EOF) {
+            character_count++;
+            if (isprint(next_character_in_file)) 
+                is_last_line_empty = false;
+            if (next_character_in_file == '\n') {
+                line_count++;
+                is_last_line_empty = true;
+            } 
+        }
+        fclose(filestream);
+        
+        if (character_count == 0)
+            return 0;
+        else {
+            if (isprint(next_character_in_file) || is_last_line_empty == false) {
+                return line_count;
+            } else 
+                return line_count - 1;
+        }
+    }
+    return -1;
 }
 
 
