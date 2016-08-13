fd20eb3e8ca68f393c8fa6b939b03af72d090a50
Peter Ivanics <peter.ivanics@reslink.fi>
2016-08-13 09:26:03 +0300

    Implemented add_string - memory leaks to be fixed

--- Module_4-03_strarray/src/strarray.c
+++ Module_4-03_strarray/src/strarray.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "strarray.h"
 
 /* Exercise a: Initializes the string array to contain the initial
@@ -7,9 +9,9 @@
  *      (that contains NULL)
  */
 char **init_array(void) {
-    char** pointer = malloc(sizeof(char*[1]));
+    char** pointer = malloc(sizeof(char*));
     
-    if (pointer != NULL ){
+    if (pointer != NULL){
         pointer[0] = NULL;
         return pointer;
     }
@@ -18,20 +20,53 @@ char **init_array(void) {
 /* Releases the memory used by the strings.
  */
 void free_strings(char **array) {
-//    for(int i = 0; i < sizeof(array); i++) {
-//        free(array[i]);
-//    }
+    int count = 0;
+    while(array[count] != NULL) {
+        free(array[count]);
+        count++;
+    }
+    free(array[count]);
     free(array);
+}
+
+int get_length(char** array) {
+    if (array[0] == NULL)
+        return 0;
+    
+    int cnt = 1;
+    
+    while (array[cnt] != NULL) {
+        cnt++;
+    }
+    
+    return cnt;
 }
 
 /* Exercise b: Add <string> to the end of array <array>.
  * Returns: pointer to the array after the string has been added.
  */
-char **add_string(char **array, const char *string)
-{
-    (void) array;  // replace these lines
-    (void) string;
-    return NULL;   // replace this
+char **add_string(char **array, const char *string) {
+    int count = 0;
+    
+    while (array[count] != NULL){
+        count++;
+    }
+    
+    char** new_pointer = realloc(array, sizeof(char*) * (count + 2));
+    
+    if (new_pointer != NULL) {
+        new_pointer[count] = malloc((strlen(string) + 2) * sizeof(char));
+        new_pointer[count + 1] = malloc(sizeof(char*));
+        
+        if (new_pointer[count] != NULL && new_pointer[count + 1] != NULL) {
+            strcpy(new_pointer[count], string);
+            new_pointer[count + 1] = NULL;
+            
+            return new_pointer;
+        }
+    }
+    free(new_pointer);
+    return array;
 }
 
 /* Exercise c: Convert letters of all strings in <array> to lower case.
