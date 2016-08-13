281e10e9ecbbf8ba71a8331f1c93a88e447891f7
Peter Ivanics <peter.ivanics@reslink.fi>
2016-08-13 22:59:10 +0300

    Fixed path of testi.txt

--- Module_5-02_stats/src/main.c
+++ Module_5-02_stats/src/main.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     /* You can modify this file as needed, for example to test different
      * files. If you run on command line, this function optionally reads
      * the filename from command line, if one is included. */
-    char *filename = "testi.txt";
+    char *filename = "src/testi.txt";
     if (argc > 1) {
         filename = argv[1];
     }
