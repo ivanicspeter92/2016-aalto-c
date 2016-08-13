5c3928ceecb1b40d18ca24070c0dd41a2c008c8e
Peter Ivanics <peter.ivanics@reslink.fi>
2016-08-13 22:51:08 +0300

    Implemented textdump()

--- Module_5-01_filedump/src/fileread.c
+++ Module_5-01_filedump/src/fileread.c
@@ -1,4 +1,6 @@
 #include "fileread.h"
+#include <stdio.h>
+#include <ctype.h>
 
 /* Prints the given file as text on the screen.
  * Only printable characters are shown. Non-printable characters are printed
@@ -6,10 +8,26 @@
  * 
  * Returns the number of characters read
  */
-int textdump(const char *filename)
-{
-    (void) filename;
-    return 0;
+int textdump(const char *filename) {
+    FILE* filestream = fopen(filename, "r");
+    
+    if (filestream != NULL) {
+        char next_character_in_file;
+        int count = 0;
+        
+        while((next_character_in_file = fgetc(filestream)) != EOF) {
+            if (isprint(next_character_in_file)) {
+                printf("%c", next_character_in_file);
+            } else {
+                printf("?");
+            }
+            count++;
+        }
+        
+        fclose(filestream);
+        return count;
+    }
+    return -1;
 }
 
 
